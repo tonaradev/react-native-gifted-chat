6a8253b794b375da796dcc9c1fef3e85ab3f3904
Anuj Sachdeva <anujs@tonara.com>
2021-04-26 16:12:34 +0530

    Added a new prop messageContainerRef

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -314,7 +314,7 @@ class GiftedChat extends React.Component {
 
           messages={this.getMessages()}
 
-          ref={component => this._messageContainerRef = component}
+          ref={component => this.props.messageContainerRef ? this.props.messageContainerRef = component  : this._messageContainerRef = component}
         />
         {this.renderChatFooter()}
       </AnimatedView>
@@ -557,6 +557,7 @@ GiftedChat.defaultProps = {
   onInputTextChanged: null,
   maxInputLength: null,
   forceGetKeyboardHeight: false,
+  messageContainerRef: null,
 };
 
 GiftedChat.propTypes = {
@@ -605,6 +606,12 @@ GiftedChat.propTypes = {
   onInputTextChanged: PropTypes.func,
   maxInputLength: PropTypes.number,
   forceGetKeyboardHeight: PropTypes.bool,
+  messageContainerRef: PropTypes.oneOfType([
+    // Either a function
+    PropTypes.func, 
+    // Or the instance of a DOM native element (see the note about SSR)
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+  ]),
 };
 
 export {
